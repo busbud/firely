da4b5566d7e5d9a7044203e1853793202eff6e22
Rui Mendes <ruimigfmendes@gmail.com>
2022-08-08 17:28:36 +0100

    Allow FirelyPlugin to be applied on a library; Add activateFetched after a successful fetch on InternalFirely

--- firely-plugin/src/main/java/com/busbud/android/firely/FirelyPlugin.kt
+++ firely-plugin/src/main/java/com/busbud/android/firely/FirelyPlugin.kt
@@ -21,8 +21,11 @@
 package com.busbud.android.firely
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownDomainObjectException
 import java.io.File
 
 
@@ -35,31 +38,39 @@ private const val FILE_NAME = "firely-config.json"
 class FirelyPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        val android = project.extensions.getByType(AppExtension::class.java)
-        // Add generated source
-        android.applicationVariants.all { variant ->
-            val sourceFolder = File(project.buildDir, "/generated/source/firely/${variant.dirName}")
-            var configFile = File("${project.projectDir.absolutePath}/$FILE_NAME")
-
-            if (!configFile.exists()) {
-                configFile = File("${project.projectDir.absolutePath}/${project.name}/${FILE_NAME}")
+        try {
+            project.extensions.getByType(AppExtension::class.java).applicationVariants.all { variant ->
+                addGeneratedSource(project, variant)
             }
-
-            if (!configFile.exists()) {
-                println("Unable to find ${configFile.absolutePath} in your project")
-                return@all
+        } catch (exception: UnknownDomainObjectException) {
+            project.extensions.getByType(LibraryExtension::class.java).libraryVariants.all { variant ->
+                addGeneratedSource(project, variant)
             }
-
-            val firelyTaskProvider = project.tasks.register(
-                "${TASK_NAME}${variant.name.replaceFirstChar { it.uppercaseChar() }}",
-                FirelyTask::class.java
-            ) { task ->
-                task.inputFile = configFile
-                task.outputDir = sourceFolder
-            }
-
-            firelyTaskProvider.get().outputs.upToDateWhen { false }
-            variant.registerJavaGeneratingTask(firelyTaskProvider, sourceFolder)
         }
+    }
+
+    private fun addGeneratedSource(project: Project, variant: BaseVariant) {
+        val sourceFolder = File(project.buildDir, "/generated/source/firely/${variant.dirName}")
+        var configFile = File("${project.projectDir.absolutePath}/$FILE_NAME")
+
+        if (!configFile.exists()) {
+            configFile = File("${project.projectDir.absolutePath}/${project.name}/${FILE_NAME}")
+        }
+
+        if (!configFile.exists()) {
+            println("Unable to find ${configFile.absolutePath} in your project")
+            return
+        }
+
+        val firelyTaskProvider = project.tasks.register(
+            "${TASK_NAME}${variant.name.replaceFirstChar { it.uppercaseChar() }}",
+            FirelyTask::class.java
+        ) { task ->
+            task.inputFile = configFile
+            task.outputDir = sourceFolder
+        }
+
+        firelyTaskProvider.get().outputs.upToDateWhen { false }
+        variant.registerJavaGeneratingTask(firelyTaskProvider, sourceFolder)
     }
 }
